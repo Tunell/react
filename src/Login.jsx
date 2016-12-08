ee60aa876764be75528047d1e78a2150eb4bf739
Pelle Tunell <pelle@josh.se>
2016-12-08 16:38:51 +0100

    remove login-page

--- src/Login.jsx
+++ src/Login.jsx
@@ -7,12 +7,7 @@ const Material = React.createClass({
     return (
       <div>
         <p style={{color:'red'}}>TODO: Inloggning endast demo, Lägg till lösenord till hela sidan</p>
-        <select style={{border:'1px solid red'}}>
-            <option>Typ av entrepenad</option>
-            <option>Grundläggnings-entreprenad</option>
-            <option>El-entreprenad</option>
-            <option>VVS-entreprenad</option>
-        </select>
+        
         <br/>
         <button type="submit" style={{border:'1px solid red'}}>Logga in</button>
       </div>
