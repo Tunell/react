a9c3f44a210af5093024825d807394d52d91e818
johnTunell <tunell.john@gmail.com>
2017-01-14 11:13:32 +0100

    Changd basepath

--- api/helpers/routeHandlerHelpers.js
+++ api/helpers/routeHandlerHelpers.js
@@ -1,7 +1,7 @@
 // Helper functions for the routeHandler
 const helpers = {
     // Takes a req.url and extracts the table-name
-    parseUrlToTable: url => url.split('/')[1].split('?')[0].replace('-', '_').slice(0, -1),
+    parseUrlToTable: url => url.split('/')[2].split('?')[0].replace('-', '_').slice(0, -1),
 
     // Takes a swagger.params and returns the parameters in a queryObject
     getQueryParams: (params) => {
