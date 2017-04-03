c64677aec9c1c6d68c384fd28a0b470d90013e0d
DinoTrainer <tunell.john@gmail.com>
2017-04-03 20:04:25 +0200

    More fixing of used material

--- api/helpers/select.js
+++ api/helpers/select.js
@@ -54,31 +54,95 @@ const selectCompositeMaterial = {
 
 const selectUsedMaterial = {
     all: () => {
+        let resultQueryOne;
+        let resultQueryTwo;
         return new Promise.using(getSqlConnection(), (connection) => {
         let query = String.raw
         `
-        SELECT 
-            used_material.id AS id, 
-            used_material.user_id AS user_id,
-            user.name AS user_name,
-            used_has_raw_material.raw_material_id AS used_has_material_id,
-            material.name AS used_has_material_name,
-            record_state.id AS record_state,
-            record_state.name AS record_state_name,
-            used_material.comment AS comment,
-            used_material.amount AS amount,
-            used_material.material_type_id AS material_type_id,
-            material_type.name AS material_type_name
-        FROM byggstyrning.used_material,  used_has_raw_material, raw_material, material, user, record_state, material_type
-        WHERE
-        used_material.id = used_has_raw_material.used_material_id AND
-        raw_material.id = used_has_raw_material.raw_material_id AND
-        material.id = raw_material.id AND
-        user.id = used_material.user_id AND
-        used_material.record_state_id = record_state.id AND
-        used_material.material_type_id = material_type.id
+       SELECT 
+	used_material.id AS id, 
+    used_material.user_id AS user_id,
+    user.name AS user_name,
+    used_has_raw_material.raw_material_id AS used_has_material_id,
+    material.name AS used_has_material_name,
+    record_state.id AS record_state_id,
+    record_state.name AS record_state_name,
+    used_material.comment AS comment,
+    used_material.amount AS amount,
+    used_material.material_type_id AS material_type_id,
+    material_type.name AS material_type_name,
+    recycle_type.id AS recycle_type_id,
+    recycle_type.name AS recycle_type_name,
+    unit.id AS unit_id,
+    unit.name AS unit_name
+    
+    
+FROM byggstyrning.used_material,  used_has_raw_material, raw_material, material, user, record_state, material_type, recycle_type, unit
+WHERE
+used_material.id = used_has_raw_material.used_material_id AND
+raw_material.id = used_has_raw_material.raw_material_id AND
+material.id = raw_material.id AND
+user.id = used_material.user_id AND
+used_material.record_state_id = record_state.id AND
+used_material.material_type_id = material_type.id AND
+recycle_type.id = material.id AND
+unit.id = raw_material.unit_id
 `
             return connection.query(query)
+                .then( (res) => {
+                    resultQueryOne = res;
+                    let query2 = String.raw
+                        `
+SELECT 
+	used_material.id AS id, 
+    used_material.user_id AS user_id,
+    user.name AS user_name,
+    used_has_composite_material.composite_material_id AS used_has_material_id,
+    composite_material.name AS used_has_material_name,
+    record_state.id AS record_state_id,
+    record_state.name AS record_state_name,
+    used_material.comment AS comment,
+    used_material.amount AS amount,
+    used_material.material_type_id AS material_type_id,
+    material_type.name AS material_type_name,
+    
+composite_material.name                AS composite_material_name,
+                composite_material.unit_id             AS composite_material_unit_id,
+                unit_2.name                            AS composite_material_unit_name,
+                composite_material.created             AS created,
+                composite_material.changed             AS changed,
+                composite_has_material.material_id     AS material_id,
+                material.name                          AS material_name,
+                composite_has_material.unit_id         AS material_unit_id,
+                unit_1.name                            AS material_unit_name,
+                composite_has_material.recycle_type_id AS material_recycle_type_id,
+                recycle_type.name                      AS material_recycle_type_name,
+                composite_has_material.amount          AS amount
+    
+    
+FROM byggstyrning.used_material,  used_has_composite_material, composite_material, user, record_state, material_type, composite_has_material, recycle_type, unit AS unit_1, material, unit AS unit_2
+WHERE
+used_material.id = used_has_composite_material.used_material_id AND
+composite_material.id = used_has_composite_material.composite_material_id AND
+user.id = used_material.user_id AND
+used_material.record_state_id = record_state.id AND
+used_material.material_type_id = material_type.id AND
+
+composite_material.id = composite_has_material.composite_material_id AND
+composite_has_material.recycle_type_id = recycle_type.id AND composite_has_material.unit_id = unit_1.id AND
+composite_has_material.material_id = material.id AND composite_material.unit_id = unit_2.id AND
+composite_material.user_id = user.id
+`
+                   return connection.query(query2)
+                })
+                .then( (res) => {
+                    resultQueryTwo = res;
+                    console.log(resultQueryOne)
+
+                    let composite_has_materials = createCompMaterials(resultQueryTwo, true)
+                    composite_has_materials.forEach( row => resultQueryOne.push(row))
+                    return resultQueryOne
+                })
         })
     }
 }
@@ -89,17 +153,17 @@ function findIndexId(compositeMaterial) {
 }
 
 // Given a db output, creates a composite-material that can be sent to client.
-function createCompMaterials(rows){
+function createCompMaterials(rows, isUsedMaterial){
     let compositeMaterials = []
     rows.forEach( row => {
         let compositeMaterial = {};
         let compositeHasMaterial = createCompHasMaterial(row);
         if(compositeMaterials.length === 0) {
-            compositeMaterials.push(createCompMaterial(row, compositeHasMaterial));
+            compositeMaterials.push(createCompMaterial(row, compositeHasMaterial, isUsedMaterial));
         } else {
             let indexOfId = compositeMaterials.findIndex(findIndexId, row);
             if(indexOfId === -1) {
-                compositeMaterial = createCompMaterial(row, compositeHasMaterial);
+                compositeMaterial = createCompMaterial(row, compositeHasMaterial, isUsedMaterial);
                 compositeMaterials.push(compositeMaterial);
             } else {
                 compositeMaterials[indexOfId].composite_has_materials.push(compositeHasMaterial);
@@ -125,7 +189,7 @@ function createCompHasMaterial(row) {
 }
 
 // Given a row and a compositeHasMaterial, creates a compositeMaterial with one compositeHasMaterial
-function createCompMaterial(row, compositeHasMaterial) {
+function createCompMaterial(row, compositeHasMaterial, isUsedMaterial) {
     let compositeMaterial = {
         id: row.id,
         user_id: row.user_id,
@@ -137,6 +201,18 @@ function createCompMaterial(row, compositeHasMaterial) {
         changed: row.changed,
         composite_has_materials: [compositeHasMaterial]
     }
+
+    if(isUsedMaterial) {
+        compositeMaterial.amount = row.amount
+        compositeMaterial.comment = row.comment
+        compositeMaterial.used_has_material_name = row.used_has_material_name
+        compositeMaterial.used_has_material_id = row.used_has_material_id
+        compositeMaterial.record_state_id = row.record_state_id
+        compositeMaterial.record_state_name = row.record_state_name
+        compositeMaterial.material_type_id = row.material_type_id
+        compositeMaterial.materyal_type_name = row.material_type_name
+    }
+
     return compositeMaterial;
 }
 
