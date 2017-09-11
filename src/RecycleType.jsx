212592e1788ef557b09eaa25c16aec3ad4d60da2
PELLE-STATION\Pelle <pelle.tunell@gmail.com>
2017-09-11 21:54:48 +0200

    Fix RecycleTypes order

--- src/RecycleType.jsx
+++ src/RecycleType.jsx
@@ -3,7 +3,10 @@ import CSSModules from "react-css-modules";
 import * as styles from "./RecycleType.less";
 
 function RecycleType({id,name}) {
-	const recycleText = ["invalid", "ej åter.", "åter.", "ospec."];
+	const recycleText = ["invalid", "Återvunnen", "Ej Åter.", "Återbruk"];
+	if(!id){
+		return <span>-</span>
+	}
 	if(id === 4){
 		return <img styleName="svanen" src="../public/svanen.svg"/>
 	}
