613dae3d0c07f6812864437dd4f32205faa3095e
PELLE-STATION\Pelle <pelle.tunell@gmail.com>
2017-09-14 23:34:55 +0200

    Fix RecycleTypes

--- src/RecycleType.jsx
+++ src/RecycleType.jsx
@@ -6,19 +6,22 @@ import { arrayToObject } from './functions/arrayToObject'
 
 
 function RecycleType({ id, recycleTypes }) {
-	console.log(recycleTypes)
-	const displayNameMap = {
-    'Ej återvunnet/återanvänt': 'Ej Åter',
-		'Miljöcertifierat (FSC eller Svanen)': (<img styleName="svanen" src={svanen}/>),
-		'Vet Ej': '',
-    'Återvunnet/återanvänt': 'Åter.'
+	if (!id || !recycleTypes){
+		return <span>-</span>
 	}
+	const displayNameMap = {
+        'Ej återvunnen/återbrukad/vet ej': 'Vet ej',
+		'Miljöcertifierat (FSC eller Svanen)': (<img styleName="svanen" src={svanen}/>),
+		'Återvunnen': 'Återvunnet',
+        'Återbrukat': 'Återbrukat'
+	};
 
-	const recycleTypesWithText = recycleTypes.map( recycleType => ({ ...recycleType, displayName: displayNameMap[recycleType.name] }) )
-	const recycleTypesWithTextMap = arrayToObject(recycleTypesWithText, 'id')
+	const recycleTypesWithText = recycleTypes.map( recycleType =>
+		({ ...recycleType, displayName: displayNameMap[recycleType.name] })
+	);
+	const recycleTypesWithTextMap = arrayToObject(recycleTypesWithText, 'id');
 
 
-	if (!id) return null
 	return <span>{recycleTypesWithTextMap[id].displayName}</span>
 }
 
